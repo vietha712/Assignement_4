0669f71d183eac1f55c2b3f0514e4f200608e7f7
vietha712 <hatheviet7121996@gmail.com>
2021-01-23 13:39:25 +0700

    Refine infor function.

--- dvector.cpp
+++ dvector.cpp
@@ -140,9 +140,7 @@ void VectorDouble::print(void) const
 
 void VectorDouble::info(void) const
 {
-    cout << "Vector infor contains the length of the vector and its value" << endl;
-    cout << "Vector length: " << vectorLength << endl;
-    this->print();
+    cout << "Vector length is " << vectorLength << endl;
 }
 
 /**** Operators ****/
@@ -361,94 +359,3 @@ double dot(const VectorDouble& firstVector, const VectorDouble& secondVector)
 
     return output;
 }
-#if 0
-double* allocate(int vectorLength)
-{
-    double* vector;
-
-    vector = new double [vectorLength];
-
-    return vector;
-}
-
-void deallocate(double* vector)
-{
-    delete[] vector;
-}
-
-void random(double* vector, int vectorLength)
-{
-    int i;
-
-    for(i = 0; i < vectorLength; ++i)
-    {
-        vector[i] = rand() % 10;
-    }
-}
-
-void zeros(double* vector, int vectorLength)
-{
-    int i;
-
-    for(i = 0; i < vectorLength; ++i)
-    {
-        vector[i] = 0;
-    }
-}
-
-double& set(double* vector, int vectorLength, int entry)
-{
-    assert(vectorLength > entry);
-
-    return vector[entry];
-}
-
-void print(double* vector, int vectorLength)
-{
-    int i;
-
-    for(i = 0; i < vectorLength; ++i)
-    {
-        cout << vector[i] << " ";
-    }
-    cout << endl;
-}
-
-void add(double *firstVector, 
-         int firstVectorLength, 
-         double *secondVector,
-         int secondVectorLength, 
-         double *outputVector, 
-         int outputVectorLength)
-{
-    int i;
-
-    assert(firstVectorLength == secondVectorLength);
-
-    for(i = 0; i < outputVectorLength; ++i)
-    {
-        outputVector[i] = firstVector[i] + secondVector[i];
-    }
-}
-
-double dot(double *firstVector, 
-           int firstVectorLength, 
-           double *secondVector, 
-           int secondVectorLength)
-{
-    int i;
-    double product(0.0);
-    double output;
-
-    assert(firstVectorLength == secondVectorLength);
-
-    for(i = 0; i < secondVectorLength; ++i)
-    {
-        product = firstVector[i] * secondVector[i];
-        output += product;
-    }
-
-    return output;
-}
-
-#endif
